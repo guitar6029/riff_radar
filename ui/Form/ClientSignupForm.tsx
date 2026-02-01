9a743fb3b819badef519950e81a793258a16b855
jsdev305 <jsdev305@gmail.com>
2026-02-01 14:10:01 -0500

    add svg icons for available / taken username

--- ui/Form/ClientSignupForm.tsx
+++ ui/Form/ClientSignupForm.tsx
@@ -105,6 +105,48 @@ export default function ClientSignupForm({
         />
         {errors.username?.message && <span>{errors.username.message}</span>}
 
+        {usernameConfirmed ? (
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={48}
+              height={48}
+              viewBox="0 0 48 48"
+            >
+              <circle cx={24} cy={24} r={21} fill="#4caf50"></circle>
+              <path
+                fill="#ccff90"
+                d="M34.6 14.6L21 28.2l-5.6-5.6l-2.8 2.8l8.4 8.4l16.4-16.4z"
+              ></path>
+            </svg>
+          </span>
+        ) : (
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+            >
+              <g fill="none">
+                <path
+                  fill="currentColor"
+                  fillOpacity={0.16}
+                  d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2"
+                ></path>
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeMiterlimit={10}
+                  strokeWidth={1.5}
+                  d="M19.187 5.047L4.957 19.098M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12s4.477 10 10 10s10-4.477 10-10"
+                ></path>
+              </g>
+            </svg>
+          </span>
+        )}
+
         <button
           type="button"
           className="btn"
