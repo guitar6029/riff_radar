480d071f6ec62051f1ac4424e478383dc452f271
jsdev305 <jsdev305@gmail.com>
2026-02-01 14:09:34 -0500

    updated avatar size

--- ui/Account/Account.tsx
+++ ui/Account/Account.tsx
@@ -5,7 +5,7 @@ import Logout from "./Logout";
 export default function Account({ avatarUrl }: { avatarUrl: string | null }) {
   return (
     <div className="flex items-center gap-2">
-      <div className="rounded-4xl w-25 h-25 border-2">
+      <div className="rounded-4xl w-16 h-16 border-2">
         {avatarUrl && (
           <img
             src={avatarUrl}
